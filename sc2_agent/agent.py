0e90538b9f42fba57295676ccc61647091435e63
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2024-09-05 19:08:48 -0400

    Adding manual training code to decouple from sb3 library, for logging states

--- sc2_agent/agent.py
+++ sc2_agent/agent.py
@@ -1,8 +1,8 @@
 
 import gymnasium as gym
 from sc2env.envs import SC2GymWrapper
-from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import PPO
+from sc2_agent.sb3_utils import learn
 from absl import flags
 
 
@@ -11,6 +11,6 @@ FLAGS([''])
 
 env = SC2GymWrapper(map_name="Simple64", player_race="terran", bot_race="random")
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=int(1e5))
+learn(model, total_timesteps=int(1e5))
 
 
